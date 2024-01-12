5b48652ae30bc368bd5a092239ceb9e145e83b51
kkm39 <kkm39@duke.edu>
2024-01-12 14:15:19 -0500

    made code generalizable to all vignettes

--- exp/experiment.js
+++ exp/experiment.js
@@ -1,25 +1,3 @@
-var mu_c = 1;
-var mu_a = 1;
-var sd_c = 1;
-var sd_a = 1;
-var threshold = 3;
-var n_learning = 10;
-
-var learning_params = new Array(n_learning);
-for (let i = 0; i<n_learning; i++) {
-    let c = sampleNormal(mu_c, sd_c);
-    let a = sampleNormal(mu_a, sd_a);
-    let e = c+a > threshold;
-    learning_params[i] = { trial: i+1, c: c, a: a, e: e }
-}
-
-// convert a number to a string in gallons
-function gallons(g) {
-    if (g == 1)
-        return g + ' gallon';
-    return g + ' gallons';
-}
-
 var jsPsych = initJsPsych({
     experiment_width: 600,
     on_finish: async function (data) {
@@ -52,7 +30,200 @@ var jsPsych = initJsPsych({
     }
 });
 
+  
+var mu_c = 1;
+var mu_a = 1;
+var sd_c = 1;
+var sd_a = 1;
+var threshold = 3;
+var c = 1;
+var a = 1;
+var n_learning = 10;
 
+var learning_params = new Array(n_learning);
+for (let i = 0; i<n_learning; i++) {
+    let c = sampleNormal(mu_c, sd_c);
+    let a = sampleNormal(mu_a, sd_a);
+    let e = c+a > threshold;
+    learning_params[i] = { trial: i+1, c: c, a: a, e: e }
+}
+
+
+
+/* A complete list of every possible vignette */
+var vignettes = [{
+    name: 'water plant',
+    instructions: "There are two plants, Plant A and Plant B, in the small town of Huxley. Every day, both plants send their sewage to the town's water treatment facility. The water facility is capable of filtering " + 
+    unit(threshold, 'gallon') + " of sewage per day. So, if Plant A and Plant B together produce more than " + unit(threshold, 'gallon') + 
+    " of sewage, then some pollution will leak out into the town’s river. <br><br>We will show you how much sewage each of the two plants produced on " + n_learning + 
+    " separate days. For each day, you will be asked whether some pollution will leak out into the town’s river.<br><br><strong>Please try to pay attention to how much each plant produces on average.</strong>",
+    units: "gallon",
+    learning: {
+        stim1: 'Plant A sent ' ,
+        stim2: ' of sewage to the water treatment plant. ',
+        stim3: '<p>Plant B sent ',
+        stim4:  ' of sewage to the water treatment plant.<p><strong>Did the water treatment plant leak sewage into the river today?</strong></p>',
+        alert: 'Incorrect response, please try again.\n\nRemember that the treatment plant leaks sewage whenever the total amount of sewage exceeds ' + threshold + ' gallons.',
+    },
+    man_check_c: "How many gallons of sewage does Plant A produce on average?",
+    man_check_a: "How many gallons of sewage does Plant B produce on average?",
+    judgment_stimulus: "<p>As a reminder, the water facility is capable of filtering " + unit(threshold, 'gallon') + " of sewage per day. So, if Plant A and Plant B together produce more than " + unit(threshold, 'gallon') + 
+    " of sewage, then some pollution will leak out into the town’s river. On this day, Plant A sent " + unit(c, 'gallon') + " of sewage to the water treatment plant, and Plant B sent " + unit(a, 'gallon') +
+    " of sewage to the water treatment plant. So, the town's river became polluted. <br> <br/> To what extent do you agree with the following statement? <br> <br/> Plant A producing " + unit(c, 'gallon') + " of sewage caused the town’s water to become polluted. <p/>"
+  },{
+    name: "sales" ,
+    instructions: "There are two employees, Susan and Mike, in the sales department of their paper company’s local branch. Every day, both employees try to sell as many reams of paper as possible. " +
+     "The local branch must sell over " + unit(threshold, 'ream') + " of paper in order to make any profit for their company. So, if Susan and Mike together sell at least " +
+      unit(threshold, 'ream') + " of paper, then their branch will make a profit for that day. <br><br>We will show you how many reams of paper each of the two employees sold on " + n_learning + 
+      " separate days. For each day, you will be asked whether the branch made a profit.<br><br><strong>Please try to pay attention to how many reams of paper each employee sold on average.</strong>",
+    units: "ream" ,
+    learning1: "Susan sold ",
+    learning2: " of paper for the branch.",
+    learning3: "<p>Mike sold ",
+    learning4: " of paper for the branch.",
+    learning_alert: 'Incorrect response, please try again.\n\nRemember that the local branch makes a profit whenever the total amount of paper exceeds ' + threshold + ' reams.' ,
+    man_check_c: "How many reams of paper does Susan sell on average?",
+    man_check_a: "How many reams of paper does Susan sell on average?",
+    judgment_stimulus: "As a reminder, the local branch of the company will make a profit if Susan and Mike together sell over " +
+     unit(threshold, 'ream') + " of paper. On this day, Susan sold " + unit(c, 'ream') + " of paper, and Mike sold " + unit(a, 'ream') + " of paper. So, the branch made a profit. <br> <br/> To what extent do you agree with the following statement? <br> <br/> Susan selling " + unit(c, 'ream') + " of paper caused the branch to make a profit."
+  }/*,{
+    name: "basketball games",
+    instructions: "There are two basketball players, Max and Carl, who are the two main scorers on their school’s basketball team. Every game, both players try to score as many points as possible." +
+    "The coach has promised to take the team out for ice cream after any game that they score over" + unit(threshold) + "So, if Max and Carl together score over" +
+    unit(threshold) + "points during a game, the coach will take the team out for ice cream. <br><br>We will show you how many points each of the two basketball players scored during" + [n_learning] +
+    "separate games. For each day, you will be asked whether the team went out for ice cream after the game. <br><br><strong>Please try to pay attention to how much each player scores on average. </strong>" ,
+    units: "points",
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: ,
+    learning_alert: 'Incorrect response, please try again.\n\nRemember that the team goes out for ice cream whenever the total amount of points exceeds ' + threshold + 'points'. 
+    man_check_c: "How many points does Max score on average?",
+    man_check_a: "How many points does Carl score on average?",
+    judgment_stimulus: ,
+    judgment_labels:['Not Caused by Max at All', 'Definitely Caused by Max']
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }{
+    name: ,
+    instructions: ,
+    units: ,
+    learning1: ,
+    learning2: ,
+    learning3: ,
+    learning4: . 
+    learning_alert: ,
+    man_check_c: ,
+    man_check_a: ,
+    judgment_stimulus: , 
+    judgment_labels:
+  }*/]
+
+/* Randomly assign a condition */
+var id = jsPsych.randomization.randomID();
+var vignette = jsPsych.randomization.sampleWithoutReplacement(vignettes, 1)[0];
+console.log('ID: ' + id);
+console.log('Vignette: ' + vignette.name);
+
+
+// Obtain a single version of each vignette randomly (use for within-participant design)
+//var VIGNETTES = [...new Set(v.map(x => x.vignette))].map(x => jsPsych.randomization.sampleWithoutReplacement(v.filter(o => o.vignettte == x), 1)[0]);
+
+// convert a number to a string in the correct units
+function unit(n, unit='gallon') {
+    if (n == 1)
+        return n + ' ' + unit;
+    return n + ' ' + unit + 's';
+}
 
 /* Get informed consent */
 var consent = {
@@ -136,10 +307,7 @@ var instructions = {
     type: jsPsychInstructions,
     show_clickable_nav: true,
     pages: ["<p>In this study, you will be asked to read some scenarios and to answer questions about those scenarios.</p>",
-        "There are two plants, Plant A and Plant B, in the small town of Huxley. Every day, both plants send their sewage to the town's water treatment facility. The water facility is capable of filtering " + 
-        gallons(threshold) + " of sewage per day. So, if Plant A and Plant B together produce more than " + gallons(threshold) + 
-        " of sewage, then some pollution will leak out into the town’s river. <br><br>We will show you how much sewage each of the two plants produced on " + n_learning + 
-        " separate days. For each day, you will be asked whether some pollution will leak out into the town’s river.<br><br><strong>Please try to pay attention to how much each plant produces on average.</strong>"]
+            vignette.instructions]
 }
 
 function sampleNormal(mean, sd, min = 0, max = Infinity) {
@@ -156,9 +324,7 @@ var learning = {
             type: jsPsychHtmlButtonResponse,
             choices: ['No', 'Yes'],
             stimulus: function () {
-                return '<p>Plant A sent ' + gallons(jsPsych.timelineVariable('c')) + ' of sewage to the water treatment plant.</p>' +
-                    '<p>Plant B sent ' + gallons(jsPsych.timelineVariable('a')) + ' of sewage to the water treatment plant.</p>' +
-                    '<p><strong>Did the water treatment plant leak sewage into the river today?</strong></p>';
+                return vignette.learning1 + unit(jsPsych.timelineVariable('c'), vignette.units) + vignette.learning2 + vignette.learning3 + unit(jsPsych.timelineVariable('a'), vignette.units) + vignette.learning4;
             },
             data: {
                 c: jsPsych.timelineVariable('c'),
@@ -170,7 +336,7 @@ var learning = {
             let response = data.values()[0].response;
             let e = data.values()[0].e;
             if (response != e)
-                alert('Incorrect response, please try again.\n\nRemember that the treatment plant leaks sewage whenever the total amount of sewage exceeds ' + threshold + ' gallons.');
+                alert(vignette.learning_alert);
             return response != e;
         }
     }],
@@ -182,33 +348,31 @@ var man_check = {
     timeline: [{
         type: jsPsychSurveyText,
         questions: [
-            { name: 'average_a', prompt: "How many gallons of sewage does Plant A produce on average?", required: true },
-            { name: 'average_b', prompt: "How many gallons of sewage does Plant B produce on average?", required: true }],
+            { name: 'average_c', prompt: vignette.man_check_c, required: true },
+            { name: 'average_a', prompt: vignette.man_check_a, required: true }],
         on_finish: function (data) {
             data.measure = 'manipulation_check';
+            data.response_c = parseFloat(data.response.average_c);
             data.response_a = parseFloat(data.response.average_a);
-            data.response_b = parseFloat(data.response.average_b);
         }
     }],
     loop_function: function (data) {
+        let response_c = parseFloat(data.values()[0].response_c);
+        if (isNaN(response_c)) alert("Please enter your response for the first question as a number.");
+        if (!isNaN(response_c) & (response_c <= 0 || response_c > 150)) alert("Please enter a valid number of " + vignette.units + "s");
         let response_a = parseFloat(data.values()[0].response_a);
-        if (isNaN(response_a)) alert("Please enter your response as a number.");
-        if (!isNaN(response_a) & (response_a <= 0 || response_a > 150)) alert("Please enter a valid number of gallons.");
-        let response_b = parseFloat(data.values()[0].response_b);
-        if (isNaN(response_b)) alert("Please enter your response as a number.");
-        if (!isNaN(response_b) & (response_b <= 0 || response_b > 150)) alert("Please enter a valid number of gallons.");
-        return isNaN(response_a) || response_a <= 0 || response_a > 150 || isNaN(response_b) || response_b <= 0 || response_b > 150;
+        if (isNaN(response_a)) alert("Please enter your response for the second question as a number.");
+        if (!isNaN(response_a) & (response_a <= 0 || response_a > 150)) alert("Please enter a valid number of " + vignette.units + "s");
+        return isNaN(response_c) || response_c <= 0 || response_c > 150 || isNaN(response_a) || response_a <= 0 || response_a > 150;
     }
 }
 
 /*display judgment */
 var judgment = {
     type: jsPsychHtmlSliderResponse,
-    stimulus: "<p>As a reminder, the water facility is capable of filtering " + gallons(threshold) + " of sewage per day. So, if Plant A and Plant B together produce more than " + gallons(threshold) + 
-    " of sewage, then some pollution will leak out into the town’s river. On this day, Plant A sent c gallons of sewage to the water treatment plant, and Plant B sent a gallons" +
-    " of sewage to the water treatment plant. <br> <br/> To what extent do you agree with the following statement? <br> <br/> The town’s water became polluted because Plant A produced c gallons of sewage. <p/>",
+    stimulus: vignette.judgment_stimulus,
     min: 0, max: 1, step: 'any', require_movement: true,
-    labels: ['Not Caused by Plant A at All', 'Definitely Caused by Plant A'],
+    labels: ['not at all', 'totally'],
     // Hide the slider thumb until response
     on_load: function () {
         document.getElementById('jspsych-html-slider-response-response').classList.add('hidden');
@@ -226,7 +390,7 @@ var confidence = {
     type: jsPsychHtmlSliderResponse,
     stimulus: '<p>Please rate how confident you are in your choice.</p>',
     min: 0, max: 1, step: 'any', require_movement: true,
-    labels: ['Not Confident at All', 'Extremely Confident'],
+    labels: ['not at all', 'totally'],
     // Hide the slider thumb until response
     on_load: function () {
         document.getElementById('jspsych-html-slider-response-response').classList.add('hidden');
